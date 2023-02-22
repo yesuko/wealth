bd7e2d78f2f3cc0457408ff3f1a724487b1b9d10
Yesuko <assankumdev@gmail.com>
2023-02-22 19:15:54 +0000

    Added pocket income class to pocket body file, for easy reading and access to pocket body components

--- lib/ui/screens/pocket/components/pocket_income.dart
+++ lib/ui/screens/pocket/components/pocket_income.dart
@@ -7,59 +7,7 @@ import 'package:wealth/ui/widgets/header_text.dart';
 import 'package:wealth/ui/widgets/tile.dart';
 import 'package:wealth/util.dart';
 
-class PocketIncome extends StatelessWidget {
-  const PocketIncome({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      height: size.height * 0.15,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: kPrimaryColor),
-        borderRadius: BorderRadius.circular(14),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: FittedBox(
-              child: HeaderText(
-                text:
-                    "Total:  $currencyUnit ${context.watch<IncomeSourceManager>().totalIncome}",
-                paddingHorizontal: 0,
-                size: 25,
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.centerRight,
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: InkWell(
-              onTap: () {
-                IncomeSourceBottomSheet.showViewSourceSheet(context);
-              },
-              child: const Text.rich(
-                TextSpan(
-                  text: "View Sources",
-                  style: TextStyle(
-                    color: kPrimaryColorAccent,
-                    //fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 class IncomeSourceTile extends StatelessWidget {
   const IncomeSourceTile({
--- lib/ui/screens/pocket/components/pocketbody.dart
+++ lib/ui/screens/pocket/components/pocketbody.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wealth/logic/managers/account_manager.dart';
+import 'package:wealth/logic/managers/income_source_manager.dart';
 import 'package:wealth/ui/screens/pocket/components/income_source_sheet.dart';
 import 'package:wealth/ui/screens/pocket/components/pocket_activity.dart';
 import 'package:wealth/ui/screens/pocket/components/pocket_card.dart';
@@ -44,6 +45,98 @@ class PocketLayout extends StatelessWidget {
     );
   }
 }
+
+class PocketIncome extends StatelessWidget {
+  const PocketIncome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      height: size.height * 0.15,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: kPrimaryColor),
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: FittedBox(
+              child: HeaderText(
+                text:
+                    "Total:  $currencyUnit ${context.watch<IncomeSourceManager>().totalIncome}",
+                paddingHorizontal: 0,
+                size: 25,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: InkWell(
+              onTap: () {
+                IncomeSourceBottomSheet.showViewSourceSheet(context);
+              },
+              child: const Text.rich(
+                TextSpan(
+                  text: "View Sources",
+                  style: TextStyle(
+                    color: kPrimaryColorAccent,
+                    //fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PocketAccount extends StatelessWidget {
+  const PocketAccount({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      child: Row(children: [
+        PocketCard(
+          title: Accounts.Savings.name,
+          amount: context.watch<SavingsAccountManager>().balance,
+          amountForLastMonth:
+              context.read<SavingsAccountManager>().prevMonthAmount,
+          percentage: context.read<SavingsAccountManager>().rate,
+        ),
+        PocketCard(
+          title: Accounts.Investment.name,
+          amount: context.watch<InvestmentAccountManager>().balance,
+          amountForLastMonth:
+              context.read<InvestmentAccountManager>().prevMonthAmount,
+          percentage: context.read<InvestmentAccountManager>().rate,
+        ),
+        PocketCard(
+          title: Accounts.Emergency.name,
+          amount: context.watch<EmergencyAccountManager>().balance,
+          amountForLastMonth:
+              context.read<EmergencyAccountManager>().prevMonthAmount,
+          percentage: context.read<EmergencyAccountManager>().rate,
+        )
+      ]),
+    );
+  }
+}
+
 
 class PocketActivities extends StatelessWidget {
   const PocketActivities({
@@ -91,39 +184,3 @@ class PocketActivities extends StatelessWidget {
   }
 }
 
-class PocketAccount extends StatelessWidget {
-  const PocketAccount({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      child: Row(children: [
-        PocketCard(
-          title: Accounts.Savings.name,
-          amount: context.watch<SavingsAccountManager>().balance,
-          amountForLastMonth:
-              context.read<SavingsAccountManager>().prevMonthAmount,
-          percentage: context.read<SavingsAccountManager>().rate,
-        ),
-        PocketCard(
-          title: Accounts.Investment.name,
-          amount: context.watch<InvestmentAccountManager>().balance,
-          amountForLastMonth:
-              context.read<InvestmentAccountManager>().prevMonthAmount,
-          percentage: context.read<InvestmentAccountManager>().rate,
-        ),
-        PocketCard(
-          title: Accounts.Emergency.name,
-          amount: context.watch<EmergencyAccountManager>().balance,
-          amountForLastMonth:
-              context.read<EmergencyAccountManager>().prevMonthAmount,
-          percentage: context.read<EmergencyAccountManager>().rate,
-        )
-      ]),
-    );
-  }
-}

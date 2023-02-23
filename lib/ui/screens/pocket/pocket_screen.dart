54f2d69693cfed99004b95bf189fe20cd52c005c
Yesuko <assankumdev@gmail.com>
2023-02-23 16:28:51 +0000

    updatedpockjet body name, to make it more clear to identify

--- lib/ui/screens/pocket/pocket_screen.dart
+++ lib/ui/screens/pocket/pocket_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wealth/ui/screens/pocket/components/pocketbody.dart';
+import 'package:wealth/ui/screens/pocket/components/pocket_body.dart';
 
 class PocketScreen extends StatelessWidget {
   const PocketScreen({super.key});

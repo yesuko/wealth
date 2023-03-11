aa073e7f3c6c6d09c2b9c82012e957754ef0d44d
Yesuko <assankumdev@gmail.com>
2023-03-11 22:58:37 +0000

    allow cached user to login

--- lib/ui/startapp_screen.dart
+++ lib/ui/startapp_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wealth/logic/managers/user_manager.dart';
 import 'package:wealth/ui/screens/login/login_screen.dart';
 import 'package:wealth/ui/screens/startup/updateapp_screen.dart';
+
+import 'screens/home/home_screen.dart';
 
 class StartAppScreen extends StatelessWidget {
   const StartAppScreen({super.key});
@@ -11,6 +15,8 @@ class StartAppScreen extends StatelessWidget {
 
     if (flag == true) {
       return const UpdateAppScreen();
+    } else if (context.read<UserManager>().userIsLogedIn) {
+      return const HomeScreen();
     } else {
       return const LoginScreen();
     }

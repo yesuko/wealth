38179d570691fa006dd597de05c5b138743a4f8c
Yesuko <assankumdev@gmail.com>
2023-03-11 22:11:56 +0000

    remove un used import

--- lib/ui/screens/register/components/registerbody.dart
+++ lib/ui/screens/register/components/registerbody.dart
@@ -6,7 +6,6 @@ import 'package:wealth/ui/screens/home/home_screen.dart';
 import 'package:wealth/ui/screens/login/login_screen.dart';
 import 'package:wealth/ui/screens/register/register_screen.dart';
 import 'package:wealth/ui/widgets/avatar.dart';
-import 'package:wealth/ui/widgets/bottom_sheet.dart';
 import 'package:wealth/ui/widgets/login_register_link.dart';
 import 'package:wealth/ui/widgets/messenger.dart';
 import 'package:wealth/ui/widgets/rounded_button.dart';

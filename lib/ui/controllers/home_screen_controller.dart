c77cda455e14c01486bd13becdb04f193fdf2a75
Yesuko <assankumdev@gmail.com>
2023-03-13 17:05:45 +0000

    added home screen controller

---
+++ lib/ui/controllers/home_screen_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../logic/managers/user_manager.dart';
+
+class HomeScreenController {
+  static reset({required BuildContext context}) {}
+}
